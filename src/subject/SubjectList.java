29bbb0e5c79764bf39c303fba266bb5c789e797d
h_mori <h_mori@LAPTOP-HUKMPH17>
2024-07-12 12:09:57 +0900

    a

---
+++ src/subject/SubjectList.java
@@ -0,0 +1,35 @@
+package subject;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Subject;
+import dao.SubjectDAO;
+
+@WebServlet("/subject_list")
+public class SubjectList extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            SubjectDAO subjectDAO = new SubjectDAO();
+            List<Subject> subjects = subjectDAO.selectAll();
+
+            request.setAttribute("subjects", subjects);
+            request.getRequestDispatcher("/subject_list.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
